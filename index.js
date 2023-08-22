7dc19373876baca96995f04504a813070ae75cf4
skjunior <142546585+skjunior@users.noreply.github.com>
2023-08-21 21:44:27 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+* segio krug junior
+* primeiro comentario
+*/
+console.log("olá Mundo!");
+// prompt ("sergio krug junior
+
+// variaveis
+const variavelA = 100;
+const variavelB = variavelA +50;
+console.log(variavelB);
+
+let numero = 50;
+console.log(numero);
+numero = 80
+console.log(numero);
+
+const nome = "sergio krug junior"
+const idade = 24
+console.log("Olá!", "meu nome é", nome, "e eu tenho", idade, "anos.");
+
+const nomeExercicio = "Sergio"
+const sobrenome = "krug"
+const idadeExercicio = 24
+true = "soou estudante"
+fslse = "não sou estudante"
+console.log("Olá me chamo", nomeExercicio + sobrenome, "tenho", idadeExercicio);
